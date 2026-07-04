5c161ceb164233225768ab142498b284347fb1a3
Leslie Leigh (李的序) <shrinktofit@outlook.com>
2026-07-04 20:19:10 +0800

    feat(algorithm)!: make retainIf stable (#8)

--- packages/cyclo-algorithm/src/retain-if.ts
+++ packages/cyclo-algorithm/src/retain-if.ts
@@ -1,6 +1,24 @@
-import { partition } from './partition.js';
-
 export function retainIf<T>(array: T[], predicate: (item: T, index: number, array: T[]) => boolean): void {
-  const iFalsy = partition(array, predicate);
-  array.length = iFalsy;
+  const arrayLength = array.length;
+  let write = 0;
+
+  for (; write < arrayLength; ++write) {
+    if (!predicate(array[write], write, array)) {
+      break;
+    }
+  }
+
+  if (write === arrayLength) {
+    return;
+  }
+
+  for (let read = write + 1; read < arrayLength; ++read) {
+    const item = array[read];
+    if (predicate(item, read, array)) {
+      array[write] = item;
+      ++write;
+    }
+  }
+
+  array.length = write;
 }
